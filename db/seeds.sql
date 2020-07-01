00a493da4de7aaeb2d51ff66c4f58cf122c11cbe
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-07-01 21:52:13 +0100

    schema and seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- input users into <users> table 
+INSERT INTO users(username, password)
+VALUES 
+(JohnDoe1, JohnDoe1pass),
+(JohnDoe2, JohnDoe2pass),
+(JohnDoe3, JohnDoe3pass)
+
+
+INSERT INTO users(name, muscle_group, description, equipment)
+VALUES 
+(exe1, pectoralis, bench pushups, bench),
+(exe2, biceps, EZ bar curl, bar),
+(exe3, vastus medalis, floor extension, none)
+
+
