05edc8b569f9cc9fbe9d65fbc3a05bc34656a58a
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-07-02 14:00:50 +0100

    run npm test for user.js file. Errors corrected

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require('bcryptjs');
+
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     firstName: {
       type: DataTypes.STRING,
       allowNull: false
@@ -11,8 +12,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
