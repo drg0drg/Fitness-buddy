439feca6eb69c16c2b4ab4b27bfa603f591143ca
JoeDodgson <j.dodgson@live.co.uk>
2020-07-04 18:55:20 +0100

    Added renderers for each html page in html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -20,10 +20,16 @@ module.exports = app => {
     }
     res.render('login', req);
   });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get('/members', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/members.html'));
+  });
+
+  // Search page
+  app.get('/search', (req, res) => {
+    res.render('search', req);
   });
 
   // Results page
@@ -35,5 +41,15 @@ module.exports = app => {
       { name: 'Preacher curls' }
     ];
     res.render('results', req);
+  });
+
+  // Exercises-details page
+  app.get('/exercises-details', (req, res) => {
+    res.render('exercises-details', req);
+  });
+
+  // Favourite-exercises page
+  app.get('/favourite-exe', (req, res) => {
+    res.render('favourite-exe', req);
   });
 };
