1a12a7db9d97901d728df3292a4d22ac8e820bad
Jacob Houston <jacob.houston658@gmail.com>
2020-07-04 15:39:51 +0100

    Dev merge (#32)
    * Jacob/bugfix/heroku deployment failure (#30)
    
    * updated and moved eslint to dev dependencies, moved dotenv to normal dependencies
    
    * Implimenting connect session store
    
    * Added saveUninitialized: true, removed proxy: true
    
    * testing new session store
    
    * removing session to test db conection
    
    * Made a function to convert URI to options object
    
    * Code cleanup and comments
    
    * console.log()
    
    * Updated to work on server
    
    * trying heroku config vars
    
    * Added in session options, code cleanup
    
    * Prettified Code!
    
    * Depricated function
    
    * Updated comments as per PR review
    
    * Prettified Code!
    
    Co-authored-by: Goldenaries658 <Goldenaries658@users.noreply.github.com>
    
    * Jacob/patch/html routes not returned (#31)
    
    * Formatting
    
    * returned html routes
    
    * Prettified Code!
    
    Co-authored-by: Goldenaries658 <Goldenaries658@users.noreply.github.com>
    
    Co-authored-by: Goldenaries658 <Goldenaries658@users.noreply.github.com>

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,22 +8,22 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      return res.status(200).redirect('/members');
     }
-    res.sendFile(path.join(__dirname, '../public/signup.html'));
+    return res.sendFile(path.join(__dirname, '../public/signup.html'));
   });
 
   app.get('/login', (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      return res.redirect('/members');
     }
-    res.sendFile(path.join(__dirname, '../public/login.html'));
+    return res.sendFile(path.join(__dirname, '../public/login.html'));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get('/members', isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/members.html'));
+    return res.sendFile(path.join(__dirname, '../public/members.html'));
   });
 };
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const session = require('express-session');
 const passport = require('./config/passport');
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
 const db = require('./models');
+const PORT = process.env.PORT || 8080;
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
@@ -14,9 +14,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
+  session({
+    secret: 'keyboard cat',
+    store: db.mysqlStore,
+    resave: false,
+    saveUninitialized: true
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
