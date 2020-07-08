1c3a99d1061052097c7ac7ddff4c71d6a16048d6
JoeDodgson <j.dodgson@live.co.uk>
2020-07-08 13:43:55 +0100

    Amended style of search button, tidied up client-side JS
    -Increased font size of bottom search button
    -Increase spacing between input fields
    -Removed unnecessary client-side JS files and functions

--- public/js/exerciseDetails.js
+++ public/js/exerciseDetails.js
@@ -1,17 +1,1 @@
-$(document).ready(() => {
-  // Getting references to our form and inputs
-  const logout = $('#footer-log-out');
-
-  // When user clicks Logout, we send a get request for the /logout route
-  logout.on('click', async event => {
-    event.preventDefault();
-    try {
-      await $.get('/api/logout');
-
-      // If there's an error, log the error
-    } catch (err) {
-      console.error(`ERROR - exercise-details.js - logout: ${err}`);
-      alert(`${err.status}: ${err.statusText}`);
-    }
-  });
-});
+// Event listener for removing a specific exercise from favourites
