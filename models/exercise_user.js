8f9fb5c97f8459baa5e73b1c6c740037bdd0732e
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-07-11 00:15:01 +0100

    Model FaveExercise working. Only contains exercise_id

--- models/exercise_user.js
+++ models/exercise_user.js
@@ -1,15 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const FaveExercise = sequelize.define('FaveExercise', {
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
     exercise_id: {
       type: DataTypes.INTEGER,
       allowNull: false
     }
   });
-  FaveExercise.associate = function(models) {
+  FaveExercise.associate = models => {
     FaveExercise.belongsTo(models.User, {
       foreignKey: { allowNull: false }
     });
--- models/user.js
+++ models/user.js
@@ -28,14 +28,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
 
-  User.associate = function(models) {
+  User.associate = models => {
     User.hasMany(models.FaveExercise, {
       onDelete: 'cascade'
     });
   };
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user
   // can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
